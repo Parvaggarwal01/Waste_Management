496ed724cef1231e539db1b926cba9f157d3a02b
Parvaggarawal01 <parvaggarwal130@gmail.com>
2024-04-07 19:00:40 +0530

    Adding Page 2 and 3

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-gsap.to("#nav",{
-    backgroundColor: "#FFEDF9",
-    height: "7vw",
-    duration: 0.2,
-    scrollTrigger:{
-        trigger: "#nav",
-        scroller: "body",
-        //markers: true,
-        start: "top -60%",
-        end: "top -11%",
-        scrub: 1,
-    }
-})
+// gsap.to("#nav",{
+//     backgroundColor: "#FFEDF9",
+//     height: "7vw",
+//     duration: 0.2,
+//     scrollTrigger:{
+//         trigger: "#nav",
+//         scroller: "body",
+//         //markers: true,
+//         start: "top -60%",
+//         end: "top -11%",
+//         scrub: 1,
+//     }
+// })
 
 gsap.to("#main",{
-    backgroundColor: "#f3fde8",
+    backgroundColor: "#FDFDFD",
     scrollTrigger:{
         trigger: "#main",
         scroller: "body",
